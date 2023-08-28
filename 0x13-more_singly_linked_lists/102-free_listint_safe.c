19f194eeb4d2e7de5f0bfe53aab01707aff1ff46
Mkhosi99 <mkhosishabalala2@gmail.com>
2023-08-28 19:36:26 +0200

    function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Function frees listint_t list,
+ *                     Function can also free lists with a loop
+ *
+ * Return: number of nodes freed
+ */
+
+size_t free_listint_safe(listint_t **head)
+{
+	int diffrnce_of_addr;
+	listint_t *tempo;
+	size_t add_freed = 0;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	while (*head != NULL)
+	{
+		diffrnce_of_addr = *head - (*head)->next;
+		if (diffrnce_of_addr > 0)
+		{
+			tempo = (*head)->next;
+			free(*head);
+			*head = tempo;
+			add_freed++;
+		}
+		else
+		{
+			free(*head);
+			*head = NULL;
+			add_freed++;
+			break;
+		}
+	}
+
+	*head = NULL;
+
+	return (add_freed);
+}
