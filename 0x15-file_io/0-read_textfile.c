e0c7b235f7fe905de0ccf9dc92e91950c7b9b655
Mkhosi99 <mkhosishabalala2@gmail.com>
2023-10-30 16:16:14 +0200

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
 
 /**
- * read_textfile - function reads text file print to STDOUT
- * @filename: the text file to read
- * @letters: the number of letters to read
- * Return: w if number of bytes is read and printed
- *         0 if otherwise
+ * read_textfile - will read text file and prints it to POSIX stdout
+ * @filename: points to name of the file
+ * @letters: number of letters function must read and print
+ *
+ * Return: 0 if function failure of NULL filename
+ *         w - number of bytes the function can read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	 ssize_t m, k, s;
 	char *bufr;
-	ssize_t fle_descrp;
-	ssize_t m;
-	ssize_t k;
 
-	fle_descrp = open(filename, O_RDONLY);
-	if (fle_descrp == -1)
+	if (filename == NULL)
 		return (0);
+
 	bufr = malloc(sizeof(char) * letters);
-	k = read(fle_descrp, bufr, letteres);
-	m = write(STDOUT_FILENO, bufr, k);
+	if (bufr ==  NULL)
+		return (0);
+
+	m = open(filename, O_RDONLY);
+	k = read(m, bufr, letters);
+	s = write(STDOUT_FILENO, bufr, k);
+
+	if (m == -1 || k == -1 || s == -1 || s != k)
+	{
+		free(bufr);
+		return (0);
+	}
 
 	free(bufr);
-	close(fle_descrp);
-	return (m);
+	close(m);
+
+	return (s);
+
 }
